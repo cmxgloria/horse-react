63296e9ec4131d03e66679cb334562482e933a55
Gloria Chen <cmxgloria@gmail.com>
2020-03-04 15:36:59 +1100

    update route

--- src/App.js
+++ src/App.js
@@ -1,63 +1,123 @@
 import React from "react";
 import "./App.css";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
+const horses = [
+  {
+    name: "maythehorsebewithu",
+    active: true,
+    country: "AUS",
+    yearOfBirth: 2000,
+    hourlyRate: 30,
+    numOfHoursWorked: 2000
+  },
+  {
+    name: "my wife knows everthing",
+    active: true,
+    country: "USA",
+    yearOfBirth: 2007,
+    hourlyRate: 50,
+    numOfHoursWorked: 2500
+  },
+  {
+    name: "arrrrr",
+    active: true,
+    country: "USA",
+    yearOfBirth: 2004,
+    hourlyRate: 30,
+    numOfHoursWorked: 2000
+  },
+  {
+    name: "hoof hearted",
+    active: false,
+    country: "AUS",
+    yearOfBirth: 1981,
+    hourlyRate: 88,
+    numOfHoursWorked: 900
+  },
+  {
+    name: "pony",
+    active: false,
+    country: "AUS",
+    yearOfBirth: 2003,
+    hourlyRate: 12,
+    numOfHoursWorked: 9000
+  },
+  {
+    name: "sunny boy",
+    active: true,
+    country: "AUS",
+    yearOfBirth: 2003,
+    hourlyRate: 42,
+    numOfHoursWorked: 6450
+  }
+];
 function Home(props) {
+  // const arr = [1, 2, 3, 4]; under put{arr.map(i=>)}
   return (
     <section>
       <h1>home sweet home</h1>
-      <button onClick={props.gotoAbout}>go to About</button>
+      <p>welcome to the home</p>
+      <Link to="/horses">please go to horses page</Link>
     </section>
   );
 }
 
-function About() {
+function Horses({ items }) {
+  // if no props, use items then no need props; {items} destructure items = props.items; key mainly use especially list, dynamic item
   return (
     <section>
-      <h1>about legend</h1>
-      <p>this is about ...</p>
+      <h1>horses</h1>
+      <p>horses details</p>
+      {/* no need prop.items*/}
+      {items.map(item => (
+        <Link key={item.name} to={`/horses/${item.name}`}>
+          <p>{item.name}</p>
+        </Link>
+      ))}
     </section>
   );
 }
 
-function Dodate() {
+function HorsesDetails(props) {
+  // useParams-custom hook from react-router-dom library, automatically destructe the name; hooks is kinds of closure, it remembers to the state
+  const { name } = useParams();
   return (
     <section>
-      <h1>donate</h1>
-      <p>how much donate &copy; 2020</p>
+      <h1>details</h1>
+      <h2>{name}</h2>
+      <h3>horse year of birth</h3>
     </section>
   );
 }
 
 class App extends React.Component {
-  state = { view: "home" };
-
-  gotoAbout = () => {
-    this.setState({
-      view: "about"
-    });
-  };
-  renderView = () => {
-    // switch like if else function
-    switch (this.state.view) {
-      case "home":
-        return <Home gotoAbout={this.gotoAbout} />;
-      case "about":
-        return <About />;
-      case "donate":
-        return <Dodate />;
-      default:
-        return <Home />;
-    }
-  };
   render() {
     return (
-      <div className="App">
-        {/* <Home />
-        <About />
-        <Dodate /> */}
-        {/* <button onClick={this.gotoAbout}>about</button> */}
-        {this.renderView()}
-      </div>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/horses">Horses</Link>
+        </nav>
+        {/* withdraw everything on the page */}
+        <Switch>
+          <Route path="/horses/:name">
+            <HorsesDetails />
+          </Route>
+          <Route path="/horses">
+            <Horses items={horses} />
+          </Route>
+          <Route exact path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     );
   }
 }
